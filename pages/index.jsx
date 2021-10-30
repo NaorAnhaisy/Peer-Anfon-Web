e4c3ae764ef0ee3298dc3ecac55d35432687aee0
Naor Anhaisy <naor.anhaisy123@gmail.com>
2021-10-31 00:53:42 +0300

    Test

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,8 +26,7 @@ export default function Home() {
               type="video/mp4" />
           </video>
           <div className={styles.homeTitleOverlayVideo}>
-            <h1>דלתות פנימיות מודרניות ויוקרתיות</h1>
-            <h3>שלום!</h3>
+            <h1>דלתות פנימיות מודרניות ויוקרתיות!</h1>
           </div>
         </div>
 
