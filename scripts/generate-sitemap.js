a7cf69bc0bfe866b96014dfdaec3457a5c23e06f
Naor Anhaisy <naor.anhaisy123@gmail.com>
2021-10-30 22:58:12 +0300

    feat: sitemap.xml and robots.txt

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,17 @@
+const sitemap = require('nextjs-sitemap-generator');
+const path = require('path');
+
+sitemap({
+    baseUrl: "https://www.peer-anfon.co.il",
+    pagesDirectory: path.resolve(__dirname, '../pages'),
+    targetDirectory: path.resolve(__dirname, '../public'),
+    ignoredExtensions: ["js", "css", "png", "jpeg", "xml", "icon", "json", "map"],
+    ignoredPaths: [
+        "404",
+        "index",
+        "favicon",
+    ],
+    extraPaths: [
+        "/"
+    ]
+})
