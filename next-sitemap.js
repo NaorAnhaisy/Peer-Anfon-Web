48ae6f3ae271f0d85c9638cb5aedc90c723211ac
Naor Anhaisy <naor.anhaisy123@gmail.com>
2021-10-30 17:24:21 +0300

    feat

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,16 @@
-const siteUrl = "https://www.peer-anfon.co.il";
+let policy = {
+    userAgent: "*"
+};
+
+if (process.env.NODE_ENV !== "production") {
+    polict.disallow = "/";
+}
 
 module.exports = {
-    siteUrl
+    siteUrl: "https://peer-anfon.co.il",
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [policy]
+    },
+    outDir: "./public"
 }
