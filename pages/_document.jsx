ca554ab6cbd654cd0f864a9e79d5b0c2a1e87417
Naor Anhaisy <naor.anhaisy123@gmail.com>
2021-11-03 16:32:34 +0200

    Feat: google site verification added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,16 +7,9 @@ class MyDocument extends Document {
             <Html lang="he">
                 <Head>
                     <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-                    <link rel="manifest" href="/manifest.json" />
-                    <meta name="theme-color" content="#f7f7f5" />
-                    <link rel="icon" href="/favicon.ico" />
-                    <link rel="apple-touch-icon" href="/example.png" />
                     <meta name="apple-mobile-web-app-status-bar" content="#f7f7f5" />
-                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-                        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous" />
-                    <link rel="stylesheet"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" />
-                    <link rel="canonical" href="https://peer-anfon-group.co.il/" />
+                    <meta name="theme-color" content="#f7f7f5" />
+                    <meta name="google-site-verification" content="jiVPxmLb8UJ1kJZBlhr4vjkO3AWneWeEYZnZ9j9upUE" />
                     <meta property="og:locale" content="he_IL" />
                     <meta property="og:type" content="website" />
                     <meta property="og:title" content="פאר אנפון - דלתות אוטומטיות, דלתות חשמליות אסתטיות ויוקרתיות" />
@@ -26,6 +19,14 @@ class MyDocument extends Document {
                     <meta name="twitter:card" content="summary_large_image" />
                     <meta name="twitter:title" content="פאר אנפון - דלתות אוטומטיות, דלתות חשמליות אסתטיות ויוקרתיות" />
                     <meta name="twitter:description" content="פאר אנפון - החברה המובילה בישראל ליצור והתקנת דלתות אוטומטיות וחשמליות, מערכות אינטרקום, דלתות כניסה לבניין ותיבות דואר, אמרת דלתות כניסה לבניין? אמרת פאר אנפון" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="apple-touch-icon" href="/example.png" />
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+                        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous" />
+                    <link rel="stylesheet"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" />
+                    <link rel="canonical" href="https://peer-anfon-group.co.il/" />
                 </Head>
                 <body>
                     <Main />
