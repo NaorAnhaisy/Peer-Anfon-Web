df202c75a7dd10cbcf08cbb06ebed41f6bc96f7d
Naor Anhaisy <naor.anhaisy123@gmail.com>
2021-11-16 23:19:41 +0200

    feat

--- pages/intercom/index.jsx
+++ pages/intercom/index.jsx
@@ -4,6 +4,29 @@ import styles from "../../styles/intercom.module.css";
 import { Container, Row, Col } from "react-bootstrap";
 import AOS from 'aos';
 
+const images = [
+    "/images/intercom/C-90-Keypad.jpg",
+    "/images/intercom/CD-AV-1P.jpg",
+    "/images/intercom/CD-AV-4P.jpg",
+    "/images/intercom/MKC.jpg",
+    "/images/intercom/intercom-1.png",
+    "/images/intercom/intercom-2.jpg",
+    "/images/intercom/intercom-3.png",
+    "/images/intercom/intercom-4.png",
+    "/images/intercom/intercom-5.jpg",
+    "/images/intercom/intercom-6.png",
+    "/images/intercom/intercom-7.jpg",
+    "/images/intercom/intercom-8.jpg",
+    "/images/intercom/intercom-9.jpg",
+    "/images/intercom/intercom-10.jpg",
+    "/images/intercom/intercom-11.jpg",
+    "/images/intercom/intercom-12.jpg",
+    "/images/intercom/intercom-13.jpg",
+    "/images/intercom/intercom-14.jpg",
+    "/images/intercom/intercom-15.jpg",
+    "/images/intercom/intercom-16.jpg",
+]
+
 export default function Intercom() {
     useEffect(() => {
         AOS.init({ once: true })
@@ -45,6 +68,22 @@ export default function Intercom() {
                     </Col>
                 </Row>
             </Container>
+            <Container>
+                <div className={`${styles.subTitleDiv} mb-5`}>
+                    <h3>מערכות אינטרקום מהמתקדמות בשוק​</h3>
+                    <h5>מה אנחנו מציעים?</h5>
+                </div>
+            </Container>
+            <div className={styles.gallery}>
+                {images?.map((imageSrc, i) => {
+                    return <div key={i} data-aos="fade-up" data-aos-offset="200" data-aos-duration="700" className={styles.hoverImgContainer}>
+                        <img src={imageSrc} alt={`אינטרקום ${i + 1}`} />
+                        <div className={styles.hoverImgMiddle}>
+                            <div className={styles.hoverImgText}>John Doe</div>
+                        </div>
+                    </div>
+                })}
+            </div>
         </div>
     )
 }
