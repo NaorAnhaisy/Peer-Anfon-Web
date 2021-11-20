8b28bb7dc3741bbde60033ecb40e7d56343ea476
Naor Anhaisy <naor.anhaisy123@gmail.com>
2021-11-20 21:29:18 +0200

    fix

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,30 +8,31 @@ class MyDocument extends Document {
             <Html lang="he">
                 <Head>
                     <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                    <meta name="description" content="פאר אנפון - החברה המובילה בישראל ליצור והתקנת דלתות אוטומטיות וחשמליות, מערכות אינטרקום, דלתות כניסה לבניין ותיבות דואר, אמרת דלתות כניסה לבניין? אמרת פאר אנפון" />
                     <meta name="apple-mobile-web-app-status-bar" content="#f7f7f5" />
                     <meta name="theme-color" content="#f7f7f5" />
                     <meta name="google-site-verification" content="jiVPxmLb8UJ1kJZBlhr4vjkO3AWneWeEYZnZ9j9upUE" />
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:title" content="פאר אנפון - דלתות אוטומטיות, דלתות חשמליות אסתטיות ויוקרתיות" />
+                    <meta name="twitter:description" content="פאר אנפון - החברה המובילה בישראל ליצור והתקנת דלתות אוטומטיות וחשמליות, מערכות אינטרקום, דלתות כניסה לבניין ותיבות דואר, אמרת דלתות כניסה לבניין? אמרת פאר אנפון" />
                     <meta property="og:locale" content="he_IL" />
                     <meta property="og:type" content="website" />
                     <meta property="og:title" content="פאר אנפון - דלתות אוטומטיות, דלתות חשמליות אסתטיות ויוקרתיות" />
                     <meta property="og:description" content="פאר אנפון - החברה המובילה בישראל ליצור והתקנת דלתות אוטומטיות וחשמליות, מערכות אינטרקום, דלתות כניסה לבניין ותיבות דואר, אמרת דלתות כניסה לבניין? אמרת פאר אנפון" />
                     <meta property="og:url" content="https://peer-anfon-group.co.il/" />
                     <meta property="og:site_name" content="פאר אנפון" />
-                    <meta name="twitter:card" content="summary_large_image" />
-                    <meta name="twitter:title" content="פאר אנפון - דלתות אוטומטיות, דלתות חשמליות אסתטיות ויוקרתיות" />
-                    <meta name="twitter:description" content="פאר אנפון - החברה המובילה בישראל ליצור והתקנת דלתות אוטומטיות וחשמליות, מערכות אינטרקום, דלתות כניסה לבניין ותיבות דואר, אמרת דלתות כניסה לבניין? אמרת פאר אנפון" />
+
                     <link rel="manifest" href="/manifest.json" />
                     <link rel="icon" href="/images/favicon.ico" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
-
-                    <link href="https://api.mapbox.com/mapbox-gl-js/v2.6.0/mapbox-gl.css" rel="stylesheet" />
+                    <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/v2.6.0/mapbox-gl.css" />
                     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
                         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous" />
                     <link rel="stylesheet"
                         href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" />
-                    <link rel="canonical" href="https://peer-anfon-group.co.il/" />
+                    <link rel="canonical" href="https://peer-anfon-group.co.il/" key="canonical" />
 
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script
