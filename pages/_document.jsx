e418d485537ec2ec7f2009a5c809a93f2b2ecfa9
Naor Anhaisy <naor.anhaisy123@gmail.com>
2021-12-04 14:31:04 +0200

    Feat

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -33,11 +33,118 @@ class MyDocument extends Document {
                     <link rel="stylesheet"
                         href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" />
                     <link rel="canonical" href="https://peer-anfon-group.co.il/" key="canonical" />
+                    <link rel="alternate" type="application/rss+xml" title="פאר אנפון &laquo; פיד‏" href="https://8peer.co.il/feed/" />
+                    <link rel="alternate" type="application/rss+xml" title="פאר אנפון &laquo; פיד תגובות‏" href="https://8peer.co.il/comments/feed/" />
+                    <link rel="alternate" type="application/rss+xml" title="פאר אנפון &laquo; פיד תגובות של דף בית" href="https://8peer.co.il/%d7%93%d7%a3-%d7%91%d7%99%d7%aa/feed/" />
 
+
+                    {/* Accessbility */}
                     <script async
                         src="https://cdn.enable.co.il/licenses/enable-L10108j65axynpdc-1121-24966/init.js"></script>
+
+                    {/* Structered Data - Json ld */}
+                    <script async type="application/ld+json" dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            "@context": "https://schema.org",
+                            "@graph": [
+                                {
+                                    "@type": [
+                                        "Store",
+                                        "Organization"
+                                    ],
+                                    "@id": "https://8peer.co.il/#organization",
+                                    "name": "פאר אנפון - דלתות חשמליות, אוטומטיות ותיבות דואר",
+                                    "url": "https://8peer.co.il",
+                                    "email": "info@8peer.co.il",
+                                    "address": {
+                                        "@type": "PostalAddress",
+                                        "streetAddress": "הסדנא 13, חולון",
+                                        "addressRegion": "חולון",
+                                        "postalCode": "5881407",
+                                        "addressCountry": "ישראל"
+                                    },
+                                    "logo": {
+                                        "@type": "ImageObject",
+                                        "url": "https://8peer.co.il/wp-content/uploads/2018/12/No-bg_NEW.png"
+                                    },
+                                    "openingHours": [
+                                        "Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday 09:00-17:00"
+                                    ],
+                                    "image": {
+                                        "@type": "ImageObject",
+                                        "url": "https://8peer.co.il/wp-content/uploads/2018/12/No-bg_NEW.png"
+                                    },
+                                    "telephone": "072-371-2701"
+                                },
+                                {
+                                    "@type": "WebSite",
+                                    "@id": "https://8peer.co.il/#website",
+                                    "url": "https://8peer.co.il",
+                                    "name": "פאר אנפון תעשיות בע\'\'מ",
+                                    "publisher": {
+                                        "@id": "https://8peer.co.il/#organization"
+                                    },
+                                    "inLanguage": "he-IL",
+                                    "potentialAction": {
+                                        "@type": "SearchAction",
+                                        "target": "https://8peer.co.il/?s={search_term_string}",
+                                        "query-input": "required name=search_term_string"
+                                    }
+                                },
+                                {
+                                    "@type": "ImageObject",
+                                    "@id": "https://8peer.co.il/#primaryImage",
+                                    "url": "https://8peer.co.il/wp-content/uploads/2018/11/home.jpg",
+                                    "width": 200,
+                                    "height": 200
+                                },
+                                {
+                                    "@type": "WebPage",
+                                    "@id": "https://8peer.co.il/#webpage",
+                                    "url": "https://8peer.co.il/",
+                                    "name": "פאר אנפון - דלתות אוטומטיות, דלתות חשמליות אסתטיות ויוקרתיות",
+                                    "datePublished": "2015-01-08T08:52:20+00:00",
+                                    "dateModified": "2021-08-09T10:24:37+00:00",
+                                    "isPartOf": {
+                                        "@id": "https://8peer.co.il/#website"
+                                    },
+                                    "primaryImageOfPage": {
+                                        "@id": "https://8peer.co.il/#primaryImage"
+                                    },
+                                    "inLanguage": "he-IL"
+                                },
+                                {
+                                    "@type": "Article",
+                                    "headline": "פאר אנפון - דלתות אוטומטיות, דלתות חשמליות אסתטיות ויוקרתיות",
+                                    "datePublished": "2015-01-08T08:52:20+00:00",
+                                    "dateModified": "2021-08-09T10:24:37+00:00",
+                                    "author": {
+                                        "@type": "Person",
+                                        "name": "admin"
+                                    },
+                                    "publisher": {
+                                        "@id": "https://8peer.co.il/#organization"
+                                    },
+                                    "description": "פאר אנפון - החברה המובילה בישראל ליצור והתקנת דלתות אוטומטיות וחשמליות, מערכות אינטרקום, דלתות כניסה לבניין ותיבות דואר, אמרת דלתות כניסה לבניין? אמרת פאר אנפון",
+                                    "@id": "https://8peer.co.il/#richSnippet",
+                                    "isPartOf": {
+                                        "@id": "https://8peer.co.il/#webpage"
+                                    },
+                                    "image": {
+                                        "@id": "https://8peer.co.il/#primaryImage"
+                                    },
+                                    "inLanguage": "he-IL",
+                                    "mainEntityOfPage": {
+                                        "@id": "https://8peer.co.il/#webpage"
+                                    }
+                                }]
+                        })
+                    }}
+                    />
+
+
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
-                    <script
+                    < script
                         async
                         src="https://www.googletagmanager.com/gtag/js?id=G-GGLJL2SHN4"
                     />
