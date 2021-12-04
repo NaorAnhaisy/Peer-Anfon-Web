48c5060c373738f8c994d104da5ca47922446bd2
Naor Anhaisy <naor.anhaisy123@gmail.com>
2021-12-04 22:11:43 +0200

    fix

--- components/ProjectsCard/ProjectsCard.jsx
+++ components/ProjectsCard/ProjectsCard.jsx
@@ -11,7 +11,7 @@ export default function ProjectsCard({ project, index }) {
     return (
         <div className={`${styles.item} ${styles.vFade} ${styles.item0}`}>
             <div className={`${styles.card} ${styles.cardProject}`}>
-                <Link href="/projects" passHref>
+                <Link href="/פרוייקטים" passHref>
                     <div className={`${styles.imgBox} ${(index - 1) % 3 === 0 ? styles.middle : styles.side}`} style={{ backgroundImage: `url("${project.thumbnail}")` }}>
                         <div className={styles.cardBody}>
                             <img src={project.thumbnail} alt="תמונת אווירה פרויקט 1" className={styles.srOnly} />
@@ -23,7 +23,7 @@ export default function ProjectsCard({ project, index }) {
                             <div className={styles.extraDetails}>
                                 <div className={`${styles.excerpt}`}>{project.description}</div>
                                 <div className={styles.actions}>
-                                    <Link href={`/projects/${project.projectID}`}>
+                                    <Link href={`/פרוייקטים/${project.projectID}`}>
                                         <a className={styles.linkToProject}>
                                             קרא עוד
                                         </a>
