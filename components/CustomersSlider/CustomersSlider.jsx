3f31bf5cb18c801b455407e97c13177bee743d6f
Naor Anhaisy <naor.anhaisy123@gmail.com>
2022-01-08 21:03:34 +0200

    fix

--- components/CustomersSlider/CustomersSlider.jsx
+++ components/CustomersSlider/CustomersSlider.jsx
@@ -19,19 +19,18 @@ const CUSTOMER_IMAGES = [
     { srcName: "kidder.jpg", altName: "לקוחות פאר אנפון קידר מבנים" },
     { srcName: "tel_aviv_university.jpg", altName: "לקוחות פאר אנפון אוניברסיטת תל אביב" },
     { srcName: "yospetel.jpg", altName: "לקוחות פאר אנפון בית חולים יוספטל" },
-    { srcName: "mizrahi.jpg", altName: "לקוחות פאר אנפון מזרחי טפחות" },
+    { srcName: "mivne.jpg", altName: "לקוחות פאר אנפון מבנה" },
+    { srcName: "meuhedet.jpg", altName: "לקוחות פאר אנפון מאוחדת" },
     { srcName: "prime_minister.jpg", altName: "לקוחות פאר אנפון משרד ראש הממשלה" },
     { srcName: "tidhar.jpg", altName: "לקוחות פאר אנפון תידהר" },
     { srcName: "azrieli.jpg", altName: "לקוחות פאר אנפון קניוני עזריאלי" },
     { srcName: "bar_ilan_university.jpg", altName: "לקוחות פאר אנפון אוניברסיטת בר אילן" },
-    { srcName: "meuhedet.jpg", altName: "לקוחות פאר אנפון מאוחדת" },
-    { srcName: "schneider.jpg", altName: "לקוחות פאר אנפון בית החולים שניידר" },
+    { srcName: "mizrahi.jpg", altName: "לקוחות פאר אנפון מזרחי טפחות" },
     { srcName: "police.jpg", altName: "לקוחות פאר אנפון משטרת ישראל" },
     { srcName: "ashkelonCollge.jpg", altName: "לקוחות פאר אנפון המכללה האקדמית אשקלון" },
     { srcName: "elal.jpg", altName: "לקוחות פאר אנפון אל על" },
     { srcName: "refael.jpg", altName: "לקוחות פאר אנפון רפאל" },
     { srcName: "bsr.webp", altName: "לקוחות פאר אנפון ב.ס.ר" },
-    { srcName: "ortTLV.jpg", altName: "לקוחות פאר אנפון אורט סינגלובסקי תל אביב" },
 ];
 
 export default function CustomersSlider() {
@@ -43,7 +42,7 @@ export default function CustomersSlider() {
                         return <div key={i} className={styles.slide}
                             style={{ width: "250px", height: "100px" }}>
                             <img className="undragablle-image"
-                                src={`/images/OurCustomers/new/${image.srcName}`}
+                                src={`/images/OurCustomers/${image.srcName}`}
                                 alt={image.altName} />
                         </div>
                     })}
@@ -51,7 +50,7 @@ export default function CustomersSlider() {
                         return <div key={i + CUSTOMER_IMAGES.length} className={styles.slide}
                             style={{ width: "250px", height: "100px" }}>
                             <img className="undragablle-image"
-                                src={`/images/OurCustomers/new/${image.srcName}`}
+                                src={`/images/OurCustomers/${image.srcName}`}
                                 alt={image.altName} />
                         </div>
                     })}
