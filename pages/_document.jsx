5db8bd51de4ec94e96d9f42c010a6479b8477155
Naor Anhaisy <naor.anhaisy123@gmail.com>
2022-03-01 19:44:40 +0200

    feat: Google tag manager

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -124,7 +124,7 @@ class MyDocument extends Document {
                                     "author": {
                                         "@type": "Person",
                                         "name": "admin",
-                                        "url":"https://peer-anfon-group.co.il/about-us/"
+                                        "url": "https://peer-anfon-group.co.il/about-us/"
                                     },
                                     "publisher": {
                                         "@id": "https://peer-anfon-group.co.il/#organization"
@@ -164,8 +164,34 @@ class MyDocument extends Document {
                         }}
                     />
 
+
+                    {/* Google Tag Manager */}
+
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-5VVSQD8');
+                            `,
+                        }}
+                    />
+
+                    {/* End Google Tag Manager */}
+
                 </Head>
                 <body>
+                    {/* <!-- Google Tag Manager (noscript) --> */}
+                    <noscript dangerouslySetInnerHTML={{
+                        __html: `
+                        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5VVSQD8"
+                        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                        `
+                    }}></noscript>
+
+                    {/* <!-- End Google Tag Manager (noscript) --> */}
                     <Main />
                     <NextScript />
                 </body>
